ef69156967efb4b46017a90a5521d6d67f706ee5
QkyGames <qkygames@hotmail.com>
2023-05-12 07:40:23 -0300

    Add files via upload

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -2,41 +2,66 @@ var imported = document.createElement('script');
 
 function createAkedoScript()
 {
-    imported.src = 'https://d1gexlcc9fpq0t.cloudfront.net/sdk/akedo_sdk_js.js';
+    imported.src = "https://d1gexlcc9fpq0t.cloudfront.net/sdk/akedo_sdk_js.js";
     document.head.appendChild(imported);
+	
+	imported.onload = function() {
+		  initAkedo();
+		};
 }
+createAkedoScript();
 
-createAkedoScript()
+function createDiv(){
+	var div = document.createElement("div")
+	div.setAttribute("id", "akedo-loader")
 
+	div.style.id = "akedo-loader";
+	div.style.position = "fixed";
+	div.style.width = "100%";
+	div.style.height = "100%";
+	div.style.background = "black";
+	div.style.display = "flex";
+	
 
-var div = document.createElement("div");
+	var elem = document.createElement("img");
+	elem.setAttribute("src", "https://d1gexlcc9fpq0t.cloudfront.net/sdk/logo.png");
+	elem.setAttribute("width", "150pt");
+	elem.style.margin = "auto";
+	
+	div.appendChild(elem);
+	
+	document.body.insertBefore(div, document.body.firstChild);
 
-div.id= "akedo-loader";
-//style.position: fixed;
-div.style.width = "100%";
-div.style.height = "100%";
-
-div.style.background = "black";
-div.style.display = "flex";
-
-
-var elem = document.createElement("img");
-elem.setAttribute("src", "https://d1gexlcc9fpq0t.cloudfront.net/sdk/logo.png");
-elem.setAttribute("width", "150pt");
-
-div.appendChild(elem);
-
-document.body.appendChild(div);
-
-
-function akedo_load_game_state(){
-	//console.log(callback);
-	console.log("asdasdasda");
 }
+createDiv();
 
-function test()
+
+function initAkedo()
 {
-	console.log("asdasdasdasdasdasdasdasdasdas")
+   akedo_init("com.gemu.pixel.dash", function(res) {
+	if (res) {
+	document.getElementById("akedo-loader").style.display =
+	"none";
+	}
+	});
+}
+
+
+function akedo_load_game_state(callback){
+	//console.log(callback);
+	
+}
+
+function akedo_save_game_state(state, callback){
+
+}
+
+function akedo_report_score(leaderboardId, score, callback){
+
+}
+
+function akedo_purchase(productId, callback){
+
 }
 
 
